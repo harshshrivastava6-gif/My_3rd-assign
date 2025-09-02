48cd2e0d52a49e04adb4c4e4ecb1dae4f259e160
Harsh Shrivastav <harshrajshrivastava6@gmail.com>
2025-09-02 01:58:31 -0700

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,11 @@
+
+import math
+num1 = float(input("enter a number :"))
+square_root = math.sqrt(num1)
+natural_logarithm = math.log(num1)
+sine = math.sin(num1)
+
+print("the square_root is :",math.sqrt(num1))
+print("the natural_logarithem is :",math.log(num1))
+print("the sine is :",math.sin(num1))
+
